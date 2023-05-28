f8e7a12dba80279b5f3d64aeb0cb83f724a4bb50
Arsen <Arsen.Berdnikov@mail.ru>
2023-05-28 15:53:36 +0300

    Аdded the ability to add images

---
+++ basic/models/ImageUpLoad.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+use yii\base\Model;
+use yii\web\UploadedFile;
+
+class ImageUpLoad extends Model{
+
+    public $image;
+
+    public function rules()
+    {
+        return[
+            [['image'], 'required'],
+            [['image'], 'file', 'extensions' => 'jpg,png']
+        ];
+    }
+
+    public function uploadFile(UploadedFile $file, $currentImage)
+    {
+        $this->image = $file;
+
+        if ($this->validate())
+        {
+            $this->deleteCurrentImage($currentImage);
+            return $this->saveImage();
+        }
+    }
+
+    public function getFolder()
+    {
+      return  \Yii::getAlias('@web') . 'uploads/';
+    }
+
+    public function generateFileName()
+    {
+        return strtolower(md5(uniqid($this->image->baseName)) . '.' . $this->image->extension);
+    }
+
+    public function deleteCurrentImage($currentImage)
+    {
+        if ($this->fileExist($currentImage))
+        {
+            unlink($this->getFolder() . $currentImage);
+        }
+    }
+
+    public function fileExist($currentImage)
+    {
+        if (!empty($currentImage) && $currentImage != null)
+        {
+            return file_exists($this->getFolder() . $currentImage);
+        }
+    }
+
+    public function saveImage()
+    {
+        $fileName = $this->generateFileName();
+        $this->image->saveAs($this->getFolder() . $fileName);
+        return $fileName;
+    }
+}
